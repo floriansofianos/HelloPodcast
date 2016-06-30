5829ab786c9084b57320c9077f127e74a3af0bcd
Florian Sofianos <florian.sofianos@gmail.com>
2016-06-30 22:23:19 +1000

    [HelloPodcast] ENH: First NodeJS commit

---
+++ HelloPodcast/server.js
@@ -0,0 +1,10 @@
+﻿var express = require('express');
+var port = process.env.port || 1337;
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+})
+
+app.listen(port);
